06534750ae9c77e3e528d9ec4c99ababd6fbf3fb
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-02-18 10:56:40 -0300

    Uso de la clase Scanner

--- src/genc181802/Main.java
+++ src/genc181802/Main.java
@@ -1,7 +1,18 @@
 package genc181802;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Bienvenido a las clases de Java!");
+
+        // Metodos de entrada y salida por consola
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        System.out.println("name = " + name);
+
     }
 }
