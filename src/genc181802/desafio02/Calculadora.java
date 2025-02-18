1647a5f0ed8a8cefc78781025f93f2e7b29c7abf
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-02-18 15:14:06 -0300

    Comienzo de desafio 02: Creacion de una calculadora

---
+++ src/genc181802/desafio02/Calculadora.java
@@ -0,0 +1,11 @@
+package genc181802.desafio02;
+
+public class Calculadora {
+    // Crear una calculadora que reciba input del usuario
+    // Guardar los input en variables
+    // Realizar operaciones con las variables
+
+    public static void main(String[] args) {
+
+    }
+}
