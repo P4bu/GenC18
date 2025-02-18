08d16596283af474375077f147068a6c2cdd39d6
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-02-18 12:51:37 -0300

    variables y rangos de los datos primitivos de Java

---
+++ src/genc181802/Variables.java
@@ -0,0 +1,22 @@
+package genc181802;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        // Tipos de datos Primitivos
+        // char, boolean, byte, short, int, long, float, double
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println("boolean: " + b1 + " o " + b2);
+        System.out.println("Rango de char: " + (int)Character.MIN_VALUE + " a " + (int)Character.MAX_VALUE);
+        System.out.println("Rango de byte: " + Byte.MIN_VALUE + " a " + Byte.MAX_VALUE);
+        System.out.println("Rango de short: " + Short.MIN_VALUE + " a " + Short.MAX_VALUE);
+        System.out.println("Rango de int: " + Integer.MIN_VALUE + " a " + Integer.MAX_VALUE);
+        System.out.println("Rango de long: " + Long.MIN_VALUE + " a " + Long.MAX_VALUE);
+        System.out.println("Rango de float: " + Float.MIN_VALUE + " a " + Float.MAX_VALUE);
+        System.out.println("Rango de double: " + Double.MIN_VALUE + " a " + Double.MAX_VALUE);
+
+
+    }
+}
