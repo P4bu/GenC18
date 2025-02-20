51d6bc199b678d28a99919b9f623cc14b98e80c2
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-02-20 11:48:31 -0300

    Se mejoran los textos a mostrar 2

--- src/genc181902/CalculadoraComisiones.java
+++ src/genc181902/CalculadoraComisiones.java
@@ -6,7 +6,7 @@ public class CalculadoraComisiones {
     public static void main(String[] args) {
         double ventas;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese sus ventas por favor");
+        System.out.println("Ingrese sus ventas por favor:");
 
 
         if(sc.hasNextDouble()){
@@ -14,22 +14,22 @@ public class CalculadoraComisiones {
             if(ventas > 0) {
                 if(ventas > 100000) {
                     System.out.println("Aplica comision de 30%");
-                    System.out.println("ventas = " + ventas);
-                    System.out.println("Comision = " + ventas * 0.30);
-                    System.out.println("ventas - comision = " + Math.floor(ventas - (ventas * 0.30)));
+                    System.out.println("ventas = $" + ventas);
+                    System.out.println("Comision = $" + ventas * 0.30);
+                    System.out.println("ventas - comision = $" + Math.floor(ventas - (ventas * 0.30)));
                 } else if (ventas > 50000 && ventas <= 90999) {
                     System.out.println("Aplica comision de 20%");
-                    System.out.println("ventas = " + ventas);
-                    System.out.println("Comision = " + ventas * 0.20);
-                    System.out.println("ventas - comision = " + Math.floor(ventas - (ventas * 0.20)));
+                    System.out.println("ventas = $" + ventas);
+                    System.out.println("Comision = $" + ventas * 0.20);
+                    System.out.println("ventas - comision = $" + Math.floor(ventas - (ventas * 0.20)));
                 } else if (ventas > 10001 && ventas <= 40999) {
                     System.out.println("Aplica comision de 10%");
-                    System.out.println("ventas = " + ventas);
-                    System.out.println("Comision = " + ventas * 0.10);
-                    System.out.println("ventas - comision = " + Math.floor(ventas - (ventas * 0.10)));
+                    System.out.println("ventas = $" + ventas);
+                    System.out.println("Comision = $" + ventas * 0.10);
+                    System.out.println("ventas - comision = $" + Math.floor(ventas - (ventas * 0.10)));
                 } else {
                     System.out.println("No aplica comision");
-                    System.out.println("ventas = " + ventas);
+                    System.out.println("ventas = $" + ventas);
                 }
             } else {
                 System.out.print("Las ventas no pueden ser negativas");
@@ -37,5 +37,6 @@ public class CalculadoraComisiones {
         } else {
             System.out.print("Entrada no válida. Ejecuta otra vez!!");
         }
+        sc.close();
     }
 }
