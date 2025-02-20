6b8d078de458928112429b256b04cc13d846a541
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-02-20 15:34:17 -0300

    Bucle for

--- src/genc182002/Bucles.java
+++ src/genc182002/Bucles.java
@@ -6,13 +6,15 @@ public class Bucles {
     public static void main(String[] args) {
         // While, Do While, For
 
+        //While
         int contador = 10;
         while(contador >= 0) {
             System.out.println("contador = " + contador);
             contador--;
         }
         System.out.println("\uD83D\uDE80 Despegue!!");
-        
+
+        // Do-While
         // Juego de adivinanza, del 1 a 10
         Scanner sc = new Scanner(System.in);
         int intentos = 1;
@@ -44,7 +46,21 @@ public class Bucles {
         String resultado = (win == true) ? "Ganaste!! \uD83D\uDE80 " : "Perdiste! \uD83D\uDE2D";
         System.out.println(resultado);
         System.out.println("El numero de la computadora es => " + computer);
-        sc.close();
 
+
+        // For
+        for(int i = 10; i >= 0; i--) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("Despegue! \uD83D\uDE80");
+
+        //Tabla de multiplicar
+        System.out.println("Ingresa un numero para ver su multiplicacion 1 - 10");
+        int numero = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (i * numero));
+        }
+        sc.close();
     }
 }
