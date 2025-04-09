8f4b85d0c912bf99d8d73dad80940cae2844c2a5
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-04-08 23:50:37 -0400

    comentarios del la funcion

--- src/ejercicios/NumeroPrimos.java
+++ src/ejercicios/NumeroPrimos.java
@@ -2,14 +2,17 @@ package ejercicios;
 
 public class NumeroPrimos {
     public static boolean esPrimo(int num){
+        // si el numero es menor a 1 no es primo
         if(num <= 1){
             return false;
         }
+        // si tiene divisibles hasta su raiz cuadrada (sqrt) no es primo
         for(int i = 2; i <= Math.sqrt(num); i++){
             if(num % i == 0){
                 return false;
             }
         }
+        // no cumple ninguna de las anteriores es PRIMO.
         return true;
     }
     public static void main(String[] args) {
