bac5126bea701cb628289833518f0962e40774c5
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-04-16 14:24:06 -0400

    se crea ejercicio FizzBuzz

--- src/ejercicios/ejerciciosEntrevistas/FizzBuzz.java
+++ src/ejercicios/ejerciciosEntrevistas/FizzBuzz.java
@@ -1,4 +1,31 @@
 package ejercicios.ejerciciosEntrevistas;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero = 1;
+
+        do {
+            System.out.print("Ingrese un numero: ");
+            numero = sc.nextInt();
+            for (int i = 0; i <= numero; i++){
+                if(i % 3 == 0 && i % 5 == 0){
+                    System.out.println("FizzBuzz");
+                } else if ( i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                }
+                else {
+                    System.out.println(i);
+                }
+            }
+        }
+        while(numero > 0);
+    }
 }
